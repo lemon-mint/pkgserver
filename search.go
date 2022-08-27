6f95800bfa3672e8a30a606aef0ea84f85a6412e
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-27 11:57:36 +0000

    Ignore pgx.ErrNoRows

--- search.go
+++ search.go
@@ -5,13 +5,14 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/julienschmidt/httprouter"
 )
 
 func SearchPackagesHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	query := r.URL.Query().Get("q")
 	pkgs, err := DBQueries.SearchPackages(context.Background(), query)
-	if err != nil {
+	if err != nil && err != pgx.ErrNoRows {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
